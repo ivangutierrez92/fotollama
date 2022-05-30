ff11df9182eb0955603795d5c4a16750eee15653
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-05-30 10:48:54 -0400

    base path configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  productionBrowserSourceMaps: true,
+  assetPrefix: '/fotollama/',
+  basePath: '/fotollama',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
